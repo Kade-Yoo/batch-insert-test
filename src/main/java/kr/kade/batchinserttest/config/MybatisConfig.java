c1adcb93c3a8169bd96d9f9d664cd047bd9fb1b2
Kade <yhm9207@naver.com>
2022-09-05 22:24:10 +0900

    MapperResource 사용하지 않는 로직 제거
    - MapperResource 사용하지 않는 로직 제거

--- src/main/java/kr/kade/batchinserttest/config/MybatisConfig.java
+++ src/main/java/kr/kade/batchinserttest/config/MybatisConfig.java
@@ -6,19 +6,11 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.mapper.MapperFactoryBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -30,7 +22,7 @@ public class MybatisConfig {
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setMapperLocations(resolveMapperLocations());
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
         factoryBean.setDataSource(dataSource);
         return factoryBean.getObject();
     }
@@ -40,21 +32,5 @@ public class MybatisConfig {
         MapperFactoryBean<MybatisBatchInsertMapper> factoryBean = new MapperFactoryBean<>(MybatisBatchInsertMapper.class);
         factoryBean.setSqlSessionFactory(sqlSessionFactory());
         return factoryBean;
-    }
-
-    public Resource[] resolveMapperLocations() {
-        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
-        List<String> mapperLocations = new ArrayList<>();
-        mapperLocations.add("classpath:mapper/*.xml");
-        List<Resource> resources = new ArrayList<>();
-        for (String mapperLocation : mapperLocations) {
-            try {
-                Resource[] mappers = resourceResolver.getResources(mapperLocation);
-                resources.addAll(Arrays.asList(mappers));
-            } catch (IOException e) {
-//                    log.error("Mybatis resources Get Exception Occur", e);
-            }
-        }
-        return resources.toArray(new Resource[0]);
     }
 }
